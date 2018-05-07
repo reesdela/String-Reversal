8ca33a4ac95bb39056298f7daad6511615d11c0a
spikaba <reesdela@gmail.com>
2018-05-07 16:31:51 -0500

    Add files via upload

---
+++ StringReverse.cpp
@@ -0,0 +1,30 @@
+/*
+ <Rees de la Houssaye>
+ <5/7/2018>
+ <StringReverse.cpp>
+ 
+ <a short, simple program that reverses a input string>
+*/
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    cout << "Enter a string please..." << endl;
+    string reverse;
+    getline(cin, reverse); //required for strings with more than one word
+    
+    std::string::reverse_iterator rit = reverse.rbegin(); //iterator that points to the oppsoite of
+                                                          //the beginning of the string (the end)
+    cout << "The reverse string is..." << endl;
+    
+    for(; rit != reverse.rend(); rit++)
+    {
+        cout << *rit;
+    }
+    cout << endl;
+
+}
+
